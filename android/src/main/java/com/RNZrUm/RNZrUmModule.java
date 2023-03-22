de2cefc4eaf6d8480d61febc84deac8729ac7d0a
zr <>
2023-03-22 09:31:52 +0800

    1

--- android/src/main/java/com/RNZrUm/RNZrUmModule.java
+++ android/src/main/java/com/RNZrUm/RNZrUmModule.java
@@ -2,6 +2,10 @@
 package com.RNZrUm;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -19,10 +23,15 @@ public class RNZrUmModule extends ReactContextBaseJavaModule {
   public RNZrUmModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
-    Context context = reactContext.getApplicationContext();
-    appkey = context.getApplicationInfo().metaData.getString("UM_APPKEY");
-    channel = context.getApplicationInfo().metaData.getString("UM_CHANNEL");
-    UMConfigure.preInit(reactContext,appkey,channel);
+    try {
+      ApplicationInfo appInfo = reactContext.getPackageManager().getApplicationInfo(reactContext.getPackageName(), PackageManager.GET_META_DATA);
+      Bundle bundle = appInfo.metaData;
+      appkey = bundle.getString("UM_APPKEY");
+      channel = bundle.getString("UM_CHANNEL");
+      UMConfigure.preInit(reactContext,appkey,channel);
+    } catch (PackageManager.NameNotFoundException e) {
+      Log.e("RNZrUmModule", "RNZrUmModule: "+e );
+    }
   }
 
   @Override
